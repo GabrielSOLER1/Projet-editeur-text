30dbe649d5cb428011dd22a463d546b1e3f2fc3b
GabrielSOLER1 <gabriel.soler@ynov.com>
2025-11-20 10:16:20 +0100

    first step

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const ejs = require("ejs")
+
+const server = express()
+const port = 3030
+
+server.set("view engine", "ejs")
+
+
+server.get("/", function (request, response) {
+    response.render("index", {})
+})
+
+server.listen(port, function() {
+    console.log("Server started!")
+})
